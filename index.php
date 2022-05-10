747652c32c915690232a9be0d9b2cdace533ab70
Thiago Alves <thiagoacsp@gmail.com>
2022-05-10 19:31:59 -0300

    Resolvido o conflito - retificando

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-	echo "Olá Mundo 456!";
-
-	echo "Olá Alves";
-=======
 	echo "Olá Mundo 123!";
 
 	echo "Olá Thiago";
->>>>>>> 444ef66c0f05590578e682d98736cbe6b79b6da4
 
 ?>
