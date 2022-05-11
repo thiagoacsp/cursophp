3b71c722fd6c11d72b49111d17b3d17a0ab95ff4
Thiago Alves <thiagoacsp@gmail.com>
2022-05-11 19:43:00 -0300

    Finalizando sessão de arrays

---
+++ arrays/exemplo-02.php
@@ -0,0 +1,16 @@
+<?php 
+
+$carro[0][0] = "GM";
+$carro[0][1] = "Cobalt";
+$carro[0][2] = "Onix";
+$carro[0][3] = "Camaro";
+
+$carro[1][0] = "Ford";
+$carro[1][1] = "Fiesta";
+$carro[1][2] = "Fusion";
+$carro[1][3] = "Ecosport";
+
+echo $carro[0][3] . "<br/>";
+
+echo end($carro[1]);
+ ?>
---
+++ arrays/exemplo-03.php
@@ -0,0 +1,16 @@
+<?php 
+
+$pessoas = array();
+
+array_push($pessoas, array(
+	'nome'=>'Thiago',
+	'idade'=>'36'
+));
+
+array_push($pessoas, array(
+	'nome'=>'Thatiana',
+	'idade'=>'40'
+));
+
+print_r($pessoas[0]['nome']);
+ ?>
