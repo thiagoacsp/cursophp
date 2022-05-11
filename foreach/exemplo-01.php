8c28461e874b2d34461b434b250114a678e0799b
Thiago Alves <thiagoacsp@gmail.com>
2022-05-11 19:20:40 -0300

    Finalizando sessão de foreach

---
+++ foreach/exemplo-01.php
@@ -0,0 +1,16 @@
+<?php 
+
+$meses = array(
+	"Janeiro",
+	"Fevereiro",
+	"Março"
+);
+
+foreach ($meses as $index => $mes) {
+
+	echo "Indice: " . $index . "<br/>";
+	echo "O mês é: " . $mes . "<br/>";
+
+}
+
+ ?>
