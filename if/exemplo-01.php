2d91d76a67680d1d4ab2c6298fe02234bd0a8b51
Thiago Alves <thiagoacsp@gmail.com>
2022-05-11 18:34:52 -0300

    Finalizando sessão de if

---
+++ if/exemplo-01.php
@@ -0,0 +1,31 @@
+<?php 
+
+$qualASuaIdade = 30;
+
+$idadeCrianca = 12;
+$idadeMaior = 18;
+$idadeMelhor = 65;
+
+if($qualASuaIdade < $idadeCrianca){
+
+	echo "Criança";
+
+} else if($qualASuaIdade < $idadeMaior){
+
+	echo "Adolecente";
+
+} else if($qualASuaIdade < $idadeMelhor){
+
+	echo "Adulto";
+
+} else {
+
+	echo "Idoso";
+
+}
+
+echo "<br/>";
+
+echo ($qualASuaIdade < $idadeMaior)?"Menor de Idade":"Maior de Idade";
+
+ ?>
