e39a4ba61c4ba8d3b17538d26c257fb7ed6b6979
Thiago Alves <thiagoacsp@gmail.com>
2022-05-16 21:53:43 -0300

    Seção de PDO 67

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -49,13 +49,72 @@ class Usuario{
 
 		if (count($results) > 0){
 
-			$row = $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			$this->setData($results[0]);
 		}
+	}
+
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+	}
+
+	public static function search($login){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"
+		));
+	}
+
+	public function login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if (count($results) > 0){
+
+			$this->setData($results[0]);
+			
+		} else{
+
+			throw new Exception("Login e/ou senha inválidos.");
+			
+		}
+	}
+
+	public function setData($data){
+
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new DateTime($data['dtcadastro']));
+	}
+
+	public function insert(){
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+		));
+
+		if(count($results) > 0){
+			$this->setData($results[0]);
+		}
+	}
+
+	public function __construct($login = "", $password = ""){
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
 	}
 
 	public function __toString(){
--- DAO/index.php
+++ DAO/index.php
@@ -8,10 +8,30 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
 */
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root = new Usuario();
+//Carrega uma lista de usuário
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-$root->loadById(1);
+//Carrega uma lista de usuários buscando pelo login
+// $search = Usuario::search("th");
+// echo json_encode($search);
 
-echo $root;
+//Carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("thiago", "123456");
+//echo $usuario;
+
+$aluno = new Usuario("aluno", "@luno");
+
+// $aluno->setDeslogin("aluno");
+// $aluno->setDessenha("@luno");
+
+$aluno->insert();
+
+echo $aluno;
 ?>
