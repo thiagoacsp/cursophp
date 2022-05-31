c9a21b2382a936c5d4f5d3c8eae6f202d3132632
Thiago Alves <thiagoacsp@gmail.com>
2022-05-31 19:55:10 -0300

    Finalizando seção de composer

---
+++ Composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '34e9c8f7c6ade5812a85839aa11dcc5727b668d3',
+        'name' => 'thiagoac/composer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'dev_requirement' => false,
+        ),
+        'thiagoac/composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '34e9c8f7c6ade5812a85839aa11dcc5727b668d3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
